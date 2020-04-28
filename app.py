21a39ec00fc1020690a64cd325a4e99467c1b060
Omarb28 <omarb28@gmail.com>
2020-04-28 08:28:46 +0000

    sqlalchemy config

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vagrant:vagrant@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return '<Todo %s, %s>' % (self.id, self.description)
+    #return 'hello'
+
+db.create_all()
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=[
-    {'description': 'Todo 1'},
-    {'description': 'Todo 2'},
-    {'description': 'Todo 3'},
-  ])
+  return render_template('index.html', data=Todo.query.all())
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000)
