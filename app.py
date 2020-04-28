c2054df534f0bc5680896806a457d0dd45cdfdba
Omarb28 <omarb28@gmail.com>
2020-04-28 09:51:42 +0000

    use ajax to add todo items

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -22,12 +22,13 @@ def index():
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.form.get('description', '')
+  description = request.get_json()['description']
   todo = Todo(description=description)
   db.session.add(todo)
   db.session.commit()
-  # return render_template('index.html', data=Todo.query.all())
-  return redirect(url_for('index'))
+  return jsonify({
+    'description': todo.description
+  })
 
 
 if __name__ == '__main__':
