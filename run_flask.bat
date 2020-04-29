50ccb19a0c12cabc5d307b661c81b212f3f3e478
Omarb28 <omarb28@gmail.com>
2020-04-29 12:23:10 +0000

    change to script that runs flask

--- run_flask.bat
+++ run_flask.bat
@@ -1,2 +1,2 @@
 export FLASK_ENV=development
-FLASK_APP=app.py FLASK_DEBUG=true flask run --host=0.0.0.0 --port=8000
+FLASK_APP=app.py FLASK_DEBUG=true python3 -m flask run --host=0.0.0.0 --port=8000
